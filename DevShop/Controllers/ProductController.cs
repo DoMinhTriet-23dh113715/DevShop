850872966c4a2966392ccbc8827d409e7d4e734d
Đỗ Minh Triết-23dh113715 <23dh113715@st.huflit.edu.vn>
2024-10-16 23:30:28 +0700

    Add project files.

---
+++ DevShop/Controllers/ProductController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DevShop1.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult ProductDetail()
+        {
+            return View();
+        }
+        public ActionResult FeaturedDetail()
+        {
+            return View();
+        }
+        public ActionResult WorkDetail()
+        {
+            return View();
+        }
+        public ActionResult EntertainmentDetail()
+        {
+            return View();
+        }
+        public ActionResult StudyDetail()
+        {
+            return View();
+        }
+        public ActionResult NewDetail()
+        {
+            return View();
+        }
+        public ActionResult SteamDetail()
+        {
+            return View();
+        }
+        public ActionResult SpotifyDetail()
+        {
+            return View();
+        }
+        public ActionResult WalletDetail()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail2()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail3()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail4()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail5()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail6()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail7()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail8()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail9()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail10()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail11()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail12()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail13()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail14()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail15()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail16()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail17()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail18()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail19()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail20()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail21()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail22()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail23()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail24()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail25()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail26()
+        {
+            return View();
+        }
+        public ActionResult ProductDetail27()
+        {
+            return View();
+        }
+    }
+}
